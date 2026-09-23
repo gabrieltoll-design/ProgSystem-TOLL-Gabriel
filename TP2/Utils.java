d446f3a1497326740b5562d021bbbdd95e9c3439
gabrieltoll-design <gabriel.toll@iut-rodez.fr>
2026-09-23 18:09:58 +0200

    Feat : TP2 , readInt , writeInt , readShort , writeShort

--- TP2/Utils.java
+++ TP2/Utils.java
@@ -1,27 +1,37 @@
+package gabriel.TOLL;
 
 public class Utils {
 
     public static int writeInt(byte[] memory, int offset, int value) {
-        memory[0] = (byte) value v & 0xFF;
-        memory[1] = (byte) value >> 8 & 0xFF;
-        memory[2] = (byte) value >> 16 & 0xFF;
-        memory[3] = (byte) value >> 24 & 0xFF;
-
+        memory[offset] = (byte)(value & 0xFF);
+        memory[offset+1] = (byte)(value >> 8 & 0xFF);
+        memory[offset+2] = (byte)(value >> 16 & 0xFF);
+        memory[offset+3] = (byte)(value >> 24 & 0xFF);
         return 4;
     }
 
     public static int readInt(byte[] memory, int offset) {
-        // TODO: Reconstituer le int sur 4 octets.
-        return 0;
+
+        int p1 = (int)memory[offset];
+        int p2 = (int)memory[offset+1] << 8;
+        int p3 = (int)memory[offset+2] << 16;
+        int p4 = (int)memory[offset+3] << 24;
+
+        return p1|p2|p3|p4;
     }
 
     public static int writeShort(byte[] memory, int offset, short value) {
-        // TODO: Écrire les 2 octets de 'value'.
+
+        memory[offset] = (byte)(value & 0xFF);
+        memory[offset+1] = (byte)(value >> 8 & 0xFF);
+
         return 2;
     }
 
     public static short readShort(byte[] memory, int offset) {
-        // TODO: Lire le short sur 2 octets.
-        return 0;
+        short p1 = (short)memory[offset];
+        short p2 = (short)(memory[offset+1] << 8);
+
+        return (short)(p1|p2) ;
     }
 }
